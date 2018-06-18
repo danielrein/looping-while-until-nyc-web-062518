a8aeba732eb75a1fed382026b1a75dfd54d103b7
Daniel Ran <danielr1735@gmail.com>
2018-06-18 19:37:01 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,7 +2,6 @@
 def using_until(phrase = 'Wingardium Leviosa\n')
         levitation_force = 6
         until levitation_force == 10
-          string += phrase
-          levitation_force += 1
         puts phrase
+        levitation_force += 1
 end
